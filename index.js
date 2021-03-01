487f5769b15822cbfe4b1dc73849b6058b94b586
tjennings343 <tjennings343@gmail.com>
2021-03-01 11:20:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function(){
+    textChange()
+})
+
+function textChange(){
+   let newText = document.getElementById('text')
+   newText.innerHTML = 'This is really cool!'
+}
